c38e88c74fb534813faf8f51b352c397b2f8a951
Kuljeet Pindoria <kuljeet.pindoria2017@gmail.com>
2021-07-22 21:45:07 +0530

    Sign Up Using Firestore Database

--- app/src/main/java/com/example/mytalk/activites/MainActivity.java
+++ app/src/main/java/com/example/mytalk/activites/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.mytalk;
+package com.example.mytalk.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.mytalk.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/mytalk/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.example.mytalk.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignIn";
+    public static final String KEY_USERID = "userId";
+    public static final String KEY_IMAGE = "image";
+}
